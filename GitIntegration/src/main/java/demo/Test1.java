e0b3e3abca4f95fef441d96dea31961fcabf6d2f
Vrushali <Vrushali@DESKTOP-H4LHD4U>
2020-04-16 13:51:34 +0530

    changes

--- GitIntegration/src/main/java/demo/Test1.java
+++ GitIntegration/src/main/java/demo/Test1.java
@@ -1,10 +1,9 @@
 package demo;
-import org.testng.annotations.*;
+//import org.testng.annotations.*;
 
 public class Test1 {
 
-	@Test
-	public void f() {
-		System.out.println("Git Integration with Jenkins");
+	public static void main(String []args) {
+		System.out.println("Git integration fail");
 	}
 }
